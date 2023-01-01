5ce52e507264dfd64ba6f1f555b9a5b319fed0e7
Etty Sekhon <etty.sekhon@gmail.com>
2023-01-01 22:19:24 +0000

    feat: add storybook 7

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -5,6 +5,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],

90c7686c2597a0fcbad3c6fd83644eee0a907786
Etty Sekhon <etty.sekhon@gmail.com>
2023-01-03 09:17:14 +0000

    fix: storybook 7 to ref ui

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -17,5 +17,5 @@ module.exports = {
   },
   "docs": {
     "autodocs": "tag"
-  }
+  },
 }
--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -1,3 +1,8 @@
+import "./globals.css";
+// include styles from the ui package
+import "ui/styles.css";
+import 'tailwindcss/tailwind.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
